937e0f34ea63887ea2f31264ba30b5ac5a575cfc
ilCame <ale.camer.pv@gmail.com>
2022-01-13 15:50:17 +0100

    program without the comments

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -14,23 +14,9 @@ public class SfgDiApplication {
 
 		MyController myController = (MyController) ctx.getBean("myController");
 
-		/* posso chiedere al contesto una istanza del mio controller, faccio un cast
-		* perche' mi restituisce un tipo object e poi posso utilizzare il metodo getBean
-		* dandogli il nome del bean. e questo e' come chiediamo al contesto di spring.*/
-
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
-
-		/*non abbiamo dovuto creare una nuova istanza con new perche' sara' spring a creare quella istanza
-		* poi mettimela nel contesto.
-		*
-		* quindi in linea 14 diciamo a spring di volere una istanza di questo oggetto, quindi
-		* dammi una istanza di questo bean dal contesto
-		*
-		* la costruzione di myController e' controllata da Spring Framework
-		*
-		* per la dependency injection devo fare in modo che sia il framework a creare e fornire gli oggetti instanziati*/
 	}
 
 }
