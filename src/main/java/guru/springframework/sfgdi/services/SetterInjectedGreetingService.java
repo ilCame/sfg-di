5bb07f327a7978b4e8f2f1b46c438f49affda20f
ilCame <ale.camer.pv@gmail.com>
2022-01-14 09:40:05 +0100

    returned to a state of error and commented the section with the error

--- src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/SetterInjectedGreetingService.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+/*il problema e' qua, non ho settato questa classe come un servizio quindi
+* spring non la riconosce e non riesce a gestirla. sarebbe da annotare come @Service*/
+
 public class SetterInjectedGreetingService implements GreetingService{
 
     @Override
