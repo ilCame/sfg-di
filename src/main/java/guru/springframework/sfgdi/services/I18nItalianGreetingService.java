2ff574ab3fc0f0d27e00c9d090f2937f2acece70
ilCame <ale.camer.pv@gmail.com>
2022-01-14 10:47:20 +0100

    lesson 39 done

--- src/main/java/guru/springframework/sfgdi/services/I18nItalianGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nItalianGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"IT", "default"}) //stiamo fornendo una lista di valori, in questo modo qunando facciamo partire il programma utilizzera' questo bean
+@Profile({"IT", "default"})
 @Service ("i18nService")
 public class I18nItalianGreetingService implements GreetingService{
     @Override
