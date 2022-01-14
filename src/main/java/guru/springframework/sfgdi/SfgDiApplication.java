374170e32149f70feeec3f1721541c1391bf74cd
ilCame <ale.camer.pv@gmail.com>
2022-01-14 10:34:00 +0100

    lesson 38 done, added 2 kind of profile to the program, an IT profile and an EN profile

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.springframework.sfgdi;
 
-import guru.springframework.sfgdi.controllers.ConstructorInjectionController;
-import guru.springframework.sfgdi.controllers.MyController;
-import guru.springframework.sfgdi.controllers.PropertyInjectionController;
-import guru.springframework.sfgdi.controllers.SettingInjectedController;
+import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -21,13 +18,19 @@ public class SfgDiApplication {
 		PropertyInjectionController propertyInjectionController = (PropertyInjectionController)  ctx.getBean("propertyInjectionController");
 		System.out.println(propertyInjectionController.getGreeting());
 
-		System.out.println("---- Setter ----");
+		System.out.println("----    Setter     ---");
 		SettingInjectedController settingInjectedController = (SettingInjectedController) ctx.getBean("settingInjectedController");
 		System.out.println(settingInjectedController.getGreeting());
 
 		System.out.println("---- Constructor ----");
 		ConstructorInjectionController constructorInjectionController = (ConstructorInjectionController) ctx.getBean("constructorInjectionController");
 		System.out.println(constructorInjectionController.getGreeting());
+
+		/*Per farlo funzionare dobbiamo inserire questa linea di codice (spring.profiles.active = EN) nell'application properties
+		* mi permette di cambiare il profilo, per ora ne abbiamo 2 uno EN ed uno IT*/
+		System.out.println("---- i18nController ----");
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 	}
 
---
+++ src/main/java/guru/springframework/sfgdi/services/I18nItalianGreetingService.java
@@ -0,0 +1,14 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("IT")
+@Service ("i18nService") //abbiamo dato un nome al servizio
+public class I18nItalianGreetingService implements GreetingService{
+    @Override
+    public String sayGreeting() {
+        return "Ciao Mondo - IT";
+    }
+}
