26d5c8ab5547f278a253b7322adec5a19d91e969
ilCame <ale.camer'pv@gmail.com>
2022-01-18 16:28:26 +0100

    lesson 72 done, added component scan and refactored a new package for the pets service

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,12 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+/*omettendolo non riuscirebbe a trovare i servizi, questo perche' lui cerca i servizi nel package di base
+* in questo caso gli stiamo dicendo di scannerizzare i componenti sia di questo package che del package .pet
+* */
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "guru.springframework.pet"})
 @SpringBootApplication
 public class SfgDiApplication {
 
--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.PetService;
+import guru.springframework.pet.PetService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
