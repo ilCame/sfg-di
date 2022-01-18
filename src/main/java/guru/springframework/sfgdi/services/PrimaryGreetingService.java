9e3d923412382828136119b8e114f5c04ab6a37e
ilCame <ale.camer'pv@gmail.com>
2022-01-18 17:44:58 +0100

    refactor a comment

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgdi.services;
 
-//avendolo aggiunto a greatingservicecinfig posso togliere entrambe le annotazioni
-//@Primary
-//@Service
 public class PrimaryGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
