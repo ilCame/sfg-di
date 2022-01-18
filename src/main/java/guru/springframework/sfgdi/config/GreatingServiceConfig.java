d2c9b16620fe9da5ce9b84bd7ddc02f951d57d1f
ilCame <ale.camer'pv@gmail.com>
2022-01-18 16:43:58 +0100

    lesson 73 done, added a configurator for the greatings servicies

---
+++ src/main/java/guru/springframework/sfgdi/config/GreatingServiceConfig.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*l'annotazione configuration dice a spring di controllare in questa classe
+* perche' sara' una classe di configurazione di spring*/
+@Configuration
+public class GreatingServiceConfig {
+
+    @Bean //i nome del bean che viene generato e' lo stesso del metodo
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
--- src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+//@Service posso rimuovere l'annotazione perche' la gestisco attraverso il gratingserviceconfig
 public class ConstructorGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
