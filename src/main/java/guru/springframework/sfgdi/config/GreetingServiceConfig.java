6d54a29666bbbb4527dcf381b22dc3eeec631a93
ilCame <ale.camer'pv@gmail.com>
2022-01-18 17:39:09 +0100

    lesson 75 done

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class GreetingServiceConfig {
 
-    @Bean //sto cercando di mimare con la ItalianGreetingRepo un database
+    @Bean
     ItalianGreetingRepository italianGreetingRepository(){
         return new ItalianGreetingRepositoryImpl();
     }
